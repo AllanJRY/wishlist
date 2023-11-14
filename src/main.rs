1743ee2f830e7f9867f610e7e535b87ebd8310e2
Allan <jarry.allan.dev@gmail.com>
2023-11-14 20:45:41 +0100

    feat: init axum server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::SocketAddr;
+
+use axum::{routing::get, Router, Server};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "Hello world!" }));
+
+    // todo: make the port configurable via env, which give the ability to use it
+    // in the docker config aswell.
+    let addr = SocketAddr::from(([0, 0, 0, 0], 7000));
+
+    Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
